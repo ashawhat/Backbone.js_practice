4143ee6d7f87d7de12800e0e81c6dc25fb90ba9c
ashawhat <ashlcycle@gmail.com>
2014-06-10 20:41:48 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackbonePractice::Application.config.secret_key_base = '7d7f9f9493b7cdf5456599c782a5b1639719d5a1a7bfc91673647acbdcd4cbc322d7f5aaf3c3a10a0c8bebc420f6a6bf2f94da3c9177f6bca7d9e6aade50cda6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BackbonePractice::Application.config.session_store :cookie_store, key: '_backbone_practice_session'
